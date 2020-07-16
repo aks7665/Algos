0e4375b98e1718cb6aae97f4e2e2aa0ebc0e0bed
aks7665 <aks199628@gmail.com>
2020-07-16 23:57:11 +0530

    vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    // Solution 1
+    // let vowels = 0;
+    // for (const char of str.toLowerCase()) {
+    //     if ( char === 'a' || char === 'e' || char === 'i'|| char === 'o'|| char === 'u' ) {
+    //         vowels ++;
+    //     }
+    // }
+    // return vowels;
+
+    // Solution 2
+    let vowels = 'aeiou';
+    let count = 0;
+    for (const char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count ++;
+        }
+    }
+    return count;
+}
 
 module.exports = vowels;
